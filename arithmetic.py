00cc16d3ec8b02fb036fff47a2c5421976a5f662
shahid12-byte <127465289+shahid12-byte@users.noreply.github.com>
2024-08-12 22:21:20 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter 1st number:"))
+num2=int(input("Enter 2nd number:"))
+print("Addition of",num1,"and",num2,"is",num1+num2)
+print("Subtraction of",num1,"from",num2,"is",num1-num2)
+print("Multiplication of",num1,"with",num2,"is",num1*num2)
+print("Division of",num1,"by",num2,"is",num1/num2)
+print("Modulus division of",num1,"by",num2,"is",num1%num2)
+print("Division of",num1,"by",num2,"is",num1//num2)
+
+
